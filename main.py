f94aded36a6178db3bcbf80f90f92b374cbda55f
adampodkowinski <adampodkdev@gmail.com>
2020-05-14 16:16:32 +0200

    Add on_message function
    It prints hello or member count of the server

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 
+print('Starting...')
+
 
 def read_token():
     with open('token.txt', 'r') as f:
@@ -8,6 +10,19 @@ def read_token():
 
 
 token = read_token()
+print('Read token successfully')
 
 client = discord.Client()
+
+
+@client.event
+async def on_message(message):
+    id = client.get_guild(704264027999567934)
+
+    if message.content.find('!hello') != -1 and message:
+        await message.channel.send('Hi')
+    elif message.content == '!users':
+        await message.channel.send(f'''# of Members {id.member.count}''')
+
+print('Running client...')
 client.run(token)
