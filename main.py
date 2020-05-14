0dfe21764c7089a6b54112787ad28654751c8998
adampodkowinski <adampodkdev@gmail.com>
2020-05-14 16:50:19 +0200

    Add more restrictions to on_message and add on_member_join function

--- main.py
+++ main.py
@@ -16,13 +16,24 @@ client = discord.Client()
 
 
 @client.event
+async def on_member_join(member):
+    for channel in member.guild.channels:
+        if str(channel) == 'general':
+            await channel.send(f'''Welcome to the server {member.mention}''')
+
+
+@client.event
 async def on_message(message):
     id = client.get_guild(704264027999567934)
+    channels = ['commands']
+    valid_users = ['JdMan#9101']
 
-    if message.content.find('!hello') != -1 and message:
-        await message.channel.send('Hi')
-    elif message.content == '!users':
-        await message.channel.send(f'''# of Members {id.member.count}''')
+    if str(message.channel) in channels and str(message.author) in valid_users:
+        if message.content.find('!hello') != -1 and message:
+            await message.channel.send('Hi')
+        elif message.content == '!users':
+            await message.channel.send(f'''Number of Members: {id.member_count}''')
+
 
 print('Running client...')
 client.run(token)
