3acf7d0d5e88fae1c948501b9f71ec0c4d0ce7aa
adampodkowinski <adampodkdev@gmail.com>
2020-05-16 12:46:21 +0200

    Change greeting to saying JD

--- main.py
+++ main.py
@@ -15,27 +15,34 @@ print('Read token successfully')
 client = discord.Client()
 
 
-@client.event
-async def on_member_join(member):
-    for channel in member.guild.channels:
-        if str(channel) == 'general':
-            await channel.send(f'''Welcome to the server {member.mention}''')
+# @client.event
+# async def on_member_join(member):
+# for channel in member.guild.channels:
+# if str(channel) == 'general':
+# await channel.send(f'''Welcome to the server {member.mention}''')
 
 
 @client.event
 async def on_message(message):
-    id = client.get_guild(704264027999567934)
-    channels = ['commands']
-    valid_users = ['JdMan#9101']
-
-    if str(message.channel) in channels and str(message.author) in valid_users:
-        if message.content.find('!hello') != -1 and message:
-            await message.channel.send('Hi')
-        elif message.content == '!users':
-            await message.channel.send(f'''Number of Members: {id.member_count}''')
-    else:
-        print(
-            f'''User: {message.author} tried to do command {message.content}, in channel {message.channel}''')
+    if message.content.find('jd') != -1 and message:
+        list_mess = message.content.split()
+        if len(list_mess) > 1:
+            try:
+                times = list_mess[list_mess.index('jd') + 1]
+                times = int(times)
+            except Exception:
+                times = 1
+        else:
+            times = 1
+        for _ in range(times):
+            await message.channel.send(':regional_indicator_j: :regional_indicator_d:')
+            await message.channel.send('''
+░░░░░██╗██████╗░
+░░░░░██║██╔══██╗
+░░░░░██║██║░░██║
+██╗░░██║██║░░██║
+╚█████╔╝██████╔╝
+░╚════╝░╚═════╝░''')
 
 print('Running client...')
 client.run(token)
